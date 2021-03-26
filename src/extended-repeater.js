acd5bfd1bc3bc7e61fa6002c6a236e72f361c64c
androscript <androscript@gmail.com>
2021-03-26 12:34:01 +0200

    feat: add solution for Basic JS task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,43 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let result = [];
+  let tempResult = [];
+  str = String(str);
+  if ('addition' in options) {
+    let addition = String(options.addition);
+    if ('additionRepeatTimes' in options) {
+      if (options.additionRepeatTimes > 1) {
+        for (let j = 1; j <= options.additionRepeatTimes; j++) {
+          tempResult.push(addition);
+        };
+        if ('additionSeparator' in options) {
+          str = str + tempResult.join(options.additionSeparator);
+        } else {
+          str = str + tempResult.join('|');
+          };
+      } else {
+        str = str + addition;
+      };
+    } else {
+      str = str + addition;
+    }; 
+  };
+  if ('repeatTimes' in options) {
+    if (options.repeatTimes > 1) {
+      for (let i = 1; i <= options.repeatTimes; i++) {
+        result.push(str);
+      };
+      if ('separator' in options) {
+        result = result.join(options.separator);
+      } else {
+        result = result.join('+');
+        };
+    } else {
+        result = str;
+    };
+  } else {
+      result = str;
+    }; 
+  return result;
 };
-  
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,70 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor (reverse = true) {
+    this.reverse = !reverse;
+  }
+
+  encrypt(message, key) {
+    let res = [];
+    let pos1;
+    let pos2;
+    let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    let mesArr = message.toLowerCase().split('');
+    let keyArr = key.toLowerCase().split('');
+    for (let i = 0, j = 0; i < mesArr.length; i++) {
+        if (alphabet.includes(mesArr[i])) {
+          pos1 = alphabet.indexOf(mesArr[i]);
+          if (j % keyArr.length === 0) {
+            j = 0;          
+          };
+          pos2 = alphabet.indexOf(keyArr[j]);  
+          if (pos1 + pos2 < alphabet.length) {
+            res.push(alphabet[pos1 + pos2]);
+          } else {
+            res.push(alphabet[pos1 + pos2 - alphabet.length]);
+          };
+        } else {
+          res.push(mesArr[i]);
+          j--;
+        };
+      j++;
+    };
+    if (this.reverse) {
+      return res.reverse().join('').toUpperCase();
+    }
+    return res.join('').toUpperCase();
+  }
+  
+  decrypt(message, key) {
+    let res = [];
+    let pos1;
+    let pos2;
+    let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    let mesArr = message.toLowerCase().split('');
+    let keyArr = key.toLowerCase().split('');
+    for (let i = 0, j = 0; i < mesArr.length; i++) {
+        if (alphabet.includes(mesArr[i])) {
+          pos1 = alphabet.indexOf(mesArr[i]);
+          if (j % keyArr.length === 0) {
+            j = 0;          
+          };
+          pos2 = alphabet.indexOf(keyArr[j]);          
+          if (pos1 >= pos2) {
+            res.push(alphabet[pos1 - pos2]);
+          } else {
+            res.push(alphabet[alphabet.length - pos2 + pos1]);
+          };
+        } else {
+          res.push(mesArr[i]);
+          j--;
+        };
+      j++;
+    };
+    if (this.reverse) {
+      return res.reverse().join('').toUpperCase();
+    }
+    return res.join('').toUpperCase();
   }
 }
 
